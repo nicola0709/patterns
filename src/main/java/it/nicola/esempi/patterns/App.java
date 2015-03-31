65a29490b2798991aad32e2a07ee2cc83eabd3ba
nicola <nicola@nicolacolonna.it>
2015-03-31 22:07:34 +0200

    Prova

---
+++ src/main/java/it/nicola/esempi/patterns/App.java
@@ -0,0 +1,13 @@
+package it.nicola.esempi.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
